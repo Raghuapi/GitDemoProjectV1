db07017c8afc564c38fe0fa45df2a13143326c25
raghavendra <raghavendra@SBLR-LT-0235.RGX.INT>
2022-05-20 23:25:11 +0530

    change

--- src/github/test1.java
+++ src/github/test1.java
@@ -1,11 +1,11 @@
 package github;
-
+//https://github.com/Raghuapi/GitDemoProjectV1.git    --->github url very imprtant
 public class test1 {
 	
 	public static void main(String[] args)
 	{
 		
-		String hugry= "yes";
+		String hungry= "yes";
 		if(hungry=="yes")
 		{
 			System.out.println("eat something.....");
