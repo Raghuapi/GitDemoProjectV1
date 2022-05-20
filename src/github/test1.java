19f07c120db010020eb23c8dd3c21b300741c82e
raghavendra <raghavendra@SBLR-LT-0235.RGX.INT>
2022-05-20 23:08:50 +0530

    this is ny first commit

---
+++ src/github/test1.java
@@ -0,0 +1,20 @@
+package github;
+
+public class test1 {
+	
+	public static void main(String[] args)
+	{
+		
+		String hugry= "yes";
+		if(hungry=="yes")
+		{
+			System.out.println("eat something.....");
+		}
+		else
+		{
+			System.out.println("not eating anything.....");
+		}
+		
+	}
+
+}
